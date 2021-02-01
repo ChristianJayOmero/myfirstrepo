db4233b1a7c88af34a8890502264bb78fd200e84
ChristianJayOmero <78344815+ChristianJayOmero@users.noreply.github.com>
2021-02-01 16:21:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <DOCTYPE html>
 <html lang = "en">
 <head>
-<meta charset = "utf-8>
-<meta name = "viewport" content = "width=device-width, initial-scale = 1.0">
-<title>Document</title>
+	<meta charset = "utf-8>
+	<meta name = "viewport" content = "width=device-width, initial-scale = 1.0">
+  <title>Document</title>
 </head>
 <body>
-<p>Welcome to my page Christian Jay G. Omero</p>
+  <p>Welcome to my page Christian Jay G. Omero</p>
 </body>
 </html>
